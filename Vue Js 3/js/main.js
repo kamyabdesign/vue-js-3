20e73eda2c871352a1099a8daad0aa00e51321e8
kamyabdesign <63772100+kamyabdesign@users.noreply.github.com>
2021-03-29 15:53:42 +0430

    Add files via upload

---
+++ Vue Js 3/js/main.js
@@ -0,0 +1,23 @@
+// const app = Vue.createApp({
+// 	data(){
+// 		return{
+// 			msg: "Hello Reza jan"
+// 		}
+// 	}
+// })
+// app.mount("#app")
+
+
+// 	
+// const app = Vue.createApp({
+// 	data() {
+// 		return {
+// 			course: "JavaScript",
+// 			author: "Mehran",
+// 			description: "I love JavaScripts",
+// 			video: 101,
+// 		}
+// 	}
+// }).mount("#app")
+
+
